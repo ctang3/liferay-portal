23c42d50b3959fa71a70472746785369f0a01041
Cheryl Tang <cheryl.tang@liferay.com>
2021-04-12 16:21:13 -0700

    LPS-130159 Show content for DDMFormInstanceRecord

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/change/tracking/spi/display/DDMFormInstanceRecordCTDisplayRenderer.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/change/tracking/spi/display/DDMFormInstanceRecordCTDisplayRenderer.java
@@ -16,24 +16,33 @@ package com.liferay.dynamic.data.mapping.form.web.internal.change.tracking.spi.d
 
 import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
 import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
+import com.liferay.change.tracking.spi.display.context.DisplayContext;
 import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.taglib.servlet.taglib.HTMLTag;
+import com.liferay.petra.io.unsync.UnsyncStringWriter;
 import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.servlet.PipingServletResponse;
 
 import java.util.Locale;
 
 import javax.portlet.PortletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -101,6 +110,8 @@ public class DDMFormInstanceRecordCTDisplayRenderer
 		DDMFormInstance ddmFormInstance =
 			ddmFormInstanceRecord.getFormInstance();
 
+		DisplayContext<?> displayContext = displayBuilder.getDisplayContext();
+
 		DDMFormValues ddmFormValues = ddmFormInstanceRecord.getDDMFormValues();
 
 		displayBuilder.display(
@@ -123,8 +134,57 @@ public class DDMFormInstanceRecordCTDisplayRenderer
 		).display(
 			"description",
 			ddmFormInstance.getDescription(displayBuilder.getLocale())
+		).display(
+			"content",
+			_getContent(
+				displayContext.getHttpServletRequest(),
+				displayContext.getHttpServletResponse(), ddmFormInstanceRecord,
+				displayBuilder.getLocale()),
+			false
 		);
 	}
+
+	private String _getContent(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse,
+			DDMFormInstanceRecord ddmFormInstanceRecord, Locale locale)
+		throws PortalException {
+
+		HTMLTag htmlTag = new HTMLTag();
+
+		DDMFormInstance ddmFormInstance =
+			ddmFormInstanceRecord.getFormInstance();
+
+		htmlTag.setClassNameId(
+			_classNameLocalService.getClassNameId(DDMStructure.class));
+		htmlTag.setClassPK(ddmFormInstance.getStructureId());
+		htmlTag.setDdmFormValues(ddmFormInstanceRecord.getDDMFormValues());
+		htmlTag.setGroupId(ddmFormInstanceRecord.getGroupId());
+		htmlTag.setReadOnly(true);
+		htmlTag.setRequestedLocale(locale);
+
+		try (UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter()) {
+			htmlTag.doTag(
+				httpServletRequest,
+				new PipingServletResponse(
+					httpServletResponse, unsyncStringWriter));
+
+			return unsyncStringWriter.toString();
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(exception, exception);
+			}
+		}
+
+		return null;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormInstanceRecordCTDisplayRenderer.class);
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
 
 	@Reference
 	private GroupLocalService _groupLocalService;
