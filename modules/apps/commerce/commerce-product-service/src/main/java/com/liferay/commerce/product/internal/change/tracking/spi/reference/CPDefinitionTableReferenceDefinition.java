b690bc0b1e95643cda4a3dbeb155db4be9d44994
Cheryl Tang <cheryl.tang@liferay.com>
2021-09-21 16:01:59 -0700

    COMMERCE-7204 Create TableReferenceDefinition for CPDefinition

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/change/tracking/spi/reference/CPDefinitionTableReferenceDefinition.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/change/tracking/spi/reference/CPDefinitionTableReferenceDefinition.java
@@ -17,14 +17,14 @@ package com.liferay.commerce.product.internal.change.tracking.spi.reference;
 import com.liferay.change.tracking.spi.reference.TableReferenceDefinition;
 import com.liferay.change.tracking.spi.reference.builder.ChildTableReferenceInfoBuilder;
 import com.liferay.change.tracking.spi.reference.builder.ParentTableReferenceInfoBuilder;
-import com.liferay.commerce.product.model.CPAttachmentFileEntry;
-import com.liferay.commerce.product.model.CPAttachmentFileEntryTable;
 import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPDefinitionLocalizationTable;
 import com.liferay.commerce.product.model.CPDefinitionTable;
-import com.liferay.commerce.product.service.persistence.CPAttachmentFileEntryPersistence;
-import com.liferay.document.library.kernel.model.DLFileEntryTable;
+import com.liferay.commerce.product.model.CPInstanceTable;
+import com.liferay.commerce.product.model.CProductTable;
+import com.liferay.commerce.product.service.persistence.CPDefinitionPersistence;
+import com.liferay.friendly.url.model.FriendlyURLEntryTable;
 import com.liferay.portal.kernel.model.ClassNameTable;
-import com.liferay.portal.kernel.model.WorkflowInstanceLinkTable;
 import com.liferay.portal.kernel.service.persistence.BasePersistence;
 
 import org.osgi.service.component.annotations.Component;
@@ -34,67 +34,67 @@ import org.osgi.service.component.annotations.Reference;
  * @author Cheryl Tang
  */
 @Component(enabled = false, service = TableReferenceDefinition.class)
-public class CPAttachmentFileEntryTableReferenceDefinition
-	implements TableReferenceDefinition<CPAttachmentFileEntryTable> {
+public class CPDefinitionTableReferenceDefinition
+	implements TableReferenceDefinition<CPDefinitionTable> {
 
 	@Override
 	public void defineChildTableReferences(
-		ChildTableReferenceInfoBuilder<CPAttachmentFileEntryTable>
+		ChildTableReferenceInfoBuilder<CPDefinitionTable>
 			childTableReferenceInfoBuilder) {
 
-		childTableReferenceInfoBuilder.singleColumnReference(
-			CPAttachmentFileEntryTable.INSTANCE.fileEntryId,
-			DLFileEntryTable.INSTANCE.fileEntryId
+		childTableReferenceInfoBuilder.assetEntryReference(
+			CPDefinitionTable.INSTANCE.CPDefinitionId, CPDefinition.class
+		).classNameReference(
+			CPDefinitionTable.INSTANCE.CPDefinitionId,
+			FriendlyURLEntryTable.INSTANCE.classPK, CPDefinition.class
 		).referenceInnerJoin(
 			fromStep -> fromStep.from(
-				WorkflowInstanceLinkTable.INSTANCE
+				FriendlyURLEntryTable.INSTANCE
 			).innerJoinON(
-				CPAttachmentFileEntryTable.INSTANCE,
-				CPAttachmentFileEntryTable.INSTANCE.companyId.eq(
-					WorkflowInstanceLinkTable.INSTANCE.companyId
-				).and(
-					CPAttachmentFileEntryTable.INSTANCE.groupId.eq(
-						WorkflowInstanceLinkTable.INSTANCE.groupId)
-				).and(
-					CPAttachmentFileEntryTable.INSTANCE.CPAttachmentFileEntryId.
-						eq(WorkflowInstanceLinkTable.INSTANCE.classPK)
-				)
+				CPDefinitionTable.INSTANCE,
+				CPDefinitionTable.INSTANCE.groupId.eq(
+					FriendlyURLEntryTable.INSTANCE.groupId)
 			).innerJoinON(
 				ClassNameTable.INSTANCE,
-				ClassNameTable.INSTANCE.classNameId.eq(
-					WorkflowInstanceLinkTable.INSTANCE.classNameId
+				ClassNameTable.INSTANCE.value.eq(
+					CPDefinition.class.getName()
 				).and(
-					ClassNameTable.INSTANCE.value.eq(
-						CPAttachmentFileEntry.class.getName())
+					FriendlyURLEntryTable.INSTANCE.classNameId.eq(
+						ClassNameTable.INSTANCE.classNameId)
 				)
 			)
+		).singleColumnReference(
+			CPDefinitionTable.INSTANCE.CProductId,
+			CProductTable.INSTANCE.CProductId
+		).singleColumnReference(
+			CPDefinitionTable.INSTANCE.CPDefinitionId,
+			CPDefinitionLocalizationTable.INSTANCE.CPDefinitionId
+		).singleColumnReference(
+			CPDefinitionTable.INSTANCE.CPDefinitionId,
+			CPInstanceTable.INSTANCE.CPDefinitionId
 		);
 	}
 
 	@Override
 	public void defineParentTableReferences(
-		ParentTableReferenceInfoBuilder<CPAttachmentFileEntryTable>
+		ParentTableReferenceInfoBuilder<CPDefinitionTable>
 			parentTableReferenceInfoBuilder) {
 
-		parentTableReferenceInfoBuilder.classNameReference(
-			CPAttachmentFileEntryTable.INSTANCE.classPK,
-			CPDefinitionTable.INSTANCE.CPDefinitionId, CPDefinition.class
-		).groupedModel(
-			CPAttachmentFileEntryTable.INSTANCE
-		);
+		parentTableReferenceInfoBuilder.groupedModel(
+			CPDefinitionTable.INSTANCE);
 	}
 
 	@Override
 	public BasePersistence<?> getBasePersistence() {
-		return _cpAttachmentFileEntryPersistence;
+		return _cpDefinitionPersistence;
 	}
 
 	@Override
-	public CPAttachmentFileEntryTable getTable() {
-		return CPAttachmentFileEntryTable.INSTANCE;
+	public CPDefinitionTable getTable() {
+		return CPDefinitionTable.INSTANCE;
 	}
 
 	@Reference
-	private CPAttachmentFileEntryPersistence _cpAttachmentFileEntryPersistence;
+	private CPDefinitionPersistence _cpDefinitionPersistence;
 
 }
