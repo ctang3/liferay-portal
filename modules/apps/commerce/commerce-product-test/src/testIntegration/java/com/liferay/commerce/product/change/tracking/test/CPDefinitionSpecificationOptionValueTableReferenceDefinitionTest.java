7a918edeae32f1aa3e96cd776b1eb9c417efc2ea
Cheryl Tang <cheryl.tang@liferay.com>
2021-09-24 11:09:56 -0700

    LPS-139344 Create TableReferenceDefinition, Test, CTDisplayRenderer for CPDefinitionSpecificationOptionValue

---
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/change/tracking/test/CPDefinitionSpecificationOptionValueTableReferenceDefinitionTest.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.change.tracking.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseTableReferenceDefinitionTestCase;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPOptionCategory;
+import com.liferay.commerce.product.model.CPSpecificationOption;
+import com.liferay.commerce.product.service.CPDefinitionSpecificationOptionValueLocalService;
+import com.liferay.commerce.product.service.CPOptionCategoryLocalService;
+import com.liferay.commerce.product.service.CPSpecificationOptionLocalService;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Cheryl Tang
+ */
+@RunWith(Arquillian.class)
+public class CPDefinitionSpecificationOptionValueTableReferenceDefinitionTest
+	extends BaseTableReferenceDefinitionTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_cpDefinition = CPTestUtil.addCPDefinition(group.getGroupId());
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			group.getGroupId());
+
+		_cpOptionCategory = _cpOptionCategoryLocalService.addCPOptionCategory(
+			TestPropsValues.getUserId(), RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomDouble(),
+			CPDefinitionSpecificationOptionValueTableReferenceDefinitionTest.
+				class.getSimpleName(),
+			_serviceContext);
+
+		_cpSpecificationOption =
+			_cpSpecificationOptionLocalService.addCPSpecificationOption(
+				TestPropsValues.getUserId(),
+				_cpOptionCategory.getCPOptionCategoryId(),
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomLocaleStringMap(), true,
+				CPDefinitionSpecificationOptionValueTableReferenceDefinitionTest.class.
+					getSimpleName(),
+				_serviceContext);
+	}
+
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		return _cpDefinitionSpecificationOptionValueLocalService.
+			addCPDefinitionSpecificationOptionValue(
+				_cpDefinition.getCPDefinitionId(),
+				_cpSpecificationOption.getCPSpecificationOptionId(),
+				_cpOptionCategory.getCPOptionCategoryId(),
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomDouble(), _serviceContext);
+	}
+
+	private CPDefinition _cpDefinition;
+
+	@Inject
+	private CPDefinitionSpecificationOptionValueLocalService
+		_cpDefinitionSpecificationOptionValueLocalService;
+
+	private CPOptionCategory _cpOptionCategory;
+
+	@Inject
+	private CPOptionCategoryLocalService _cpOptionCategoryLocalService;
+
+	private CPSpecificationOption _cpSpecificationOption;
+
+	@Inject
+	private CPSpecificationOptionLocalService
+		_cpSpecificationOptionLocalService;
+
+	private ServiceContext _serviceContext;
+
+}
