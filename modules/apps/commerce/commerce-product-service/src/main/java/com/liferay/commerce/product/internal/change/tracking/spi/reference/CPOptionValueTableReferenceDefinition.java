9dbfcc368935fef74c8e9d31dc73d5dd7d9215d6
Cheryl Tang <cheryl.tang@liferay.com>
2021-10-11 13:34:47 -0700

    COMMERCE-7204 Create TableReferenceDefinition for CPOptionValue

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/change/tracking/spi/reference/CPOptionValueTableReferenceDefinition.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/change/tracking/spi/reference/CPOptionValueTableReferenceDefinition.java
@@ -18,8 +18,10 @@ import com.liferay.change.tracking.spi.reference.TableReferenceDefinition;
 import com.liferay.change.tracking.spi.reference.builder.ChildTableReferenceInfoBuilder;
 import com.liferay.change.tracking.spi.reference.builder.ParentTableReferenceInfoBuilder;
 import com.liferay.commerce.product.model.CPOptionTable;
+import com.liferay.commerce.product.model.CPOptionValue;
 import com.liferay.commerce.product.model.CPOptionValueTable;
 import com.liferay.commerce.product.service.persistence.CPOptionValuePersistence;
+import com.liferay.portal.kernel.model.CompanyTable;
 import com.liferay.portal.kernel.service.persistence.BasePersistence;
 
 import org.osgi.service.component.annotations.Component;
@@ -36,6 +38,13 @@ public class CPOptionValueTableReferenceDefinition
 	public void defineChildTableReferences(
 		ChildTableReferenceInfoBuilder<CPOptionValueTable>
 			childTableReferenceInfoBuilder) {
+
+		childTableReferenceInfoBuilder.resourcePermissionReference(
+			CPOptionValueTable.INSTANCE.CPOptionValueId, CPOptionValue.class
+		).singleColumnReference(
+			CPOptionValueTable.INSTANCE.CPOptionId,
+			CPOptionTable.INSTANCE.CPOptionId
+		);
 	}
 
 	@Override
@@ -44,8 +53,12 @@ public class CPOptionValueTableReferenceDefinition
 			parentTableReferenceInfoBuilder) {
 
 		parentTableReferenceInfoBuilder.singleColumnReference(
+			CPOptionValueTable.INSTANCE.companyId,
+			CompanyTable.INSTANCE.companyId
+		).singleColumnReference(
 			CPOptionValueTable.INSTANCE.CPOptionId,
-			CPOptionTable.INSTANCE.CPOptionId);
+			CPOptionTable.INSTANCE.CPOptionId
+		);
 	}
 
 	@Override
