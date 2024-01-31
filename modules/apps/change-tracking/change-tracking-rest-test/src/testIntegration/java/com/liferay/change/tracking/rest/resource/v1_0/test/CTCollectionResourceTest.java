1e7441ea2c7e30f4384b7c9920c663f1cfacc0e9
Cheryl Tang <cheryl.tang@liferay.com>
2024-01-31 12:10:44 -0800

    LPD-15297 Implement test for new endpoint

--- modules/apps/change-tracking/change-tracking-rest-test/src/testIntegration/java/com/liferay/change/tracking/rest/resource/v1_0/test/CTCollectionResourceTest.java
+++ modules/apps/change-tracking/change-tracking-rest-test/src/testIntegration/java/com/liferay/change/tracking/rest/resource/v1_0/test/CTCollectionResourceTest.java
@@ -11,22 +11,35 @@ import com.liferay.change.tracking.model.CTPreferences;
 import com.liferay.change.tracking.rest.client.dto.v1_0.CTCollection;
 import com.liferay.change.tracking.rest.client.dto.v1_0.Status;
 import com.liferay.change.tracking.rest.client.http.HttpInvoker;
+import com.liferay.change.tracking.rest.client.pagination.Page;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.change.tracking.service.CTPreferencesLocalService;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.ws.rs.core.Response;
 
 import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -36,6 +49,13 @@ import org.junit.runner.RunWith;
 @DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class CTCollectionResourceTest extends BaseCTCollectionResourceTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Override
 	@Test
@@ -65,6 +85,68 @@ public class CTCollectionResourceTest extends BaseCTCollectionResourceTestCase {
 			ctCollectionResource.
 				getCTCollectionByExternalReferenceCodeShareLink(
 					ctCollection.getExternalReferenceCode()));
+	}
+
+	@Override
+	@Test
+	public void testGetCTCollectionsGetHistoriesPage() throws Exception {
+		Layout layout = LayoutTestUtil.addTypeContentLayout(testGroup);
+
+		try {
+			long layoutClassNameId = ClassNameLocalServiceUtil.getClassNameId(
+				Layout.class);
+			long classPK = layout.getPlid();
+
+			Page<CTCollection> page =
+				ctCollectionResource.getCTCollectionsGetHistoriesPage(
+					(int)layoutClassNameId, (int)classPK);
+
+			long totalCount = page.getTotalCount();
+
+			CTCollection ctCollection1 =
+				testGetCTCollectionsGetHistoriesPage_addCTCollection(
+					randomCTCollection());
+
+			try (SafeCloseable safeCloseable =
+					CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+						ctCollection1.getId())) {
+
+				LayoutLocalServiceUtil.updateName(
+					layout, ctCollection1.getName(),
+					layout.getDefaultLanguageId());
+			}
+
+			CTCollection ctCollection2 =
+				testGetCTCollectionsGetHistoriesPage_addCTCollection(
+					randomCTCollection());
+
+			try (SafeCloseable safeCloseable =
+					CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+						ctCollection2.getId())) {
+
+				LayoutLocalServiceUtil.updateName(
+					layout, ctCollection2.getName(),
+					layout.getDefaultLanguageId());
+			}
+
+			page = ctCollectionResource.getCTCollectionsGetHistoriesPage(
+				(int)layoutClassNameId, (int)classPK);
+
+			Assert.assertEquals(totalCount + 2, page.getTotalCount());
+
+			assertContains(ctCollection1, (List<CTCollection>)page.getItems());
+			assertContains(ctCollection2, (List<CTCollection>)page.getItems());
+			assertValid(
+				page,
+				testGetCTCollectionsGetHistoriesPage_getExpectedActions());
+
+			ctCollectionResource.deleteCTCollection(ctCollection1.getId());
+
+			ctCollectionResource.deleteCTCollection(ctCollection2.getId());
+		}
+		finally {
+			LayoutLocalServiceUtil.deleteLayout(layout);
+		}
 	}
 
 	@Override
@@ -279,25 +361,7 @@ public class CTCollectionResourceTest extends BaseCTCollectionResourceTestCase {
 			CTCollection ctCollection)
 		throws Exception {
 
-		CTCollection postCTCollection = ctCollectionResource.postCTCollection(
-			ctCollection);
-
-		com.liferay.change.tracking.model.CTCollection
-			serviceBuilderCTCollection =
-				_ctCollectionLocalService.getCTCollection(
-					postCTCollection.getId());
-
-		serviceBuilderCTCollection.setCreateDate(ctCollection.getDateCreated());
-		serviceBuilderCTCollection.setModifiedDate(
-			ctCollection.getDateModified());
-		serviceBuilderCTCollection.setShareable(true);
-
-		serviceBuilderCTCollection =
-			_ctCollectionLocalService.updateCTCollection(
-				serviceBuilderCTCollection);
-
-		return ctCollectionResource.getCTCollection(
-			serviceBuilderCTCollection.getCtCollectionId());
+		return _postShareableCTCollection(ctCollection);
 	}
 
 	@Override
@@ -305,25 +369,7 @@ public class CTCollectionResourceTest extends BaseCTCollectionResourceTestCase {
 			CTCollection ctCollection)
 		throws Exception {
 
-		CTCollection postCTCollection = ctCollectionResource.postCTCollection(
-			ctCollection);
-
-		com.liferay.change.tracking.model.CTCollection
-			serviceBuilderCTCollection =
-				_ctCollectionLocalService.getCTCollection(
-					postCTCollection.getId());
-
-		serviceBuilderCTCollection.setCreateDate(ctCollection.getDateCreated());
-		serviceBuilderCTCollection.setModifiedDate(
-			ctCollection.getDateModified());
-		serviceBuilderCTCollection.setShareable(true);
-
-		serviceBuilderCTCollection =
-			_ctCollectionLocalService.updateCTCollection(
-				serviceBuilderCTCollection);
-
-		return ctCollectionResource.getCTCollection(
-			serviceBuilderCTCollection.getCtCollectionId());
+		return _postShareableCTCollection(ctCollection);
 	}
 
 	@Override
@@ -415,6 +461,30 @@ public class CTCollectionResourceTest extends BaseCTCollectionResourceTestCase {
 			Assert.assertEquals(
 				exceptionClass.getSimpleName(), jsonObject.get("type"));
 		}
+	}
+
+	private CTCollection _postShareableCTCollection(CTCollection ctCollection)
+		throws Exception {
+
+		CTCollection postCTCollection = ctCollectionResource.postCTCollection(
+			ctCollection);
+
+		com.liferay.change.tracking.model.CTCollection
+			serviceBuilderCTCollection =
+				_ctCollectionLocalService.getCTCollection(
+					postCTCollection.getId());
+
+		serviceBuilderCTCollection.setCreateDate(ctCollection.getDateCreated());
+		serviceBuilderCTCollection.setModifiedDate(
+			ctCollection.getDateModified());
+		serviceBuilderCTCollection.setShareable(true);
+
+		serviceBuilderCTCollection =
+			_ctCollectionLocalService.updateCTCollection(
+				serviceBuilderCTCollection);
+
+		return ctCollectionResource.getCTCollection(
+			serviceBuilderCTCollection.getCtCollectionId());
 	}
 
 	@Inject
