804641179974eba81b394f7bd0957c61afedc004
Cheryl Tang <cheryl.tang@liferay.com>
2024-07-05 15:23:56 -0700

    TEMP: Attempt to fix CT Transaction failure in final delete-user call during teardown(?)

--- portal-impl/src/com/liferay/portal/service/impl/BrowserTrackerLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/BrowserTrackerLocalServiceImpl.java
@@ -5,6 +5,7 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.kernel.change.tracking.CTAware;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -14,6 +15,7 @@ import com.liferay.portal.service.base.BrowserTrackerLocalServiceBaseImpl;
 /**
  * @author Brian Wing Shun Chan
  */
+@CTAware
 public class BrowserTrackerLocalServiceImpl
 	extends BrowserTrackerLocalServiceBaseImpl {
 
