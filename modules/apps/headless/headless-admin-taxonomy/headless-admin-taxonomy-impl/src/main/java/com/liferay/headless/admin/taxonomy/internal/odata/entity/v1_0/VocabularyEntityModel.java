7c01765bda17832bdc3ff25cd4279888c6c1a999
Cheryl Tang <cheryl.tang@liferay.com>
2025-03-05 21:38:22 -0800

    LPD-49933 Implement filtering for assetTypes and assetLibraryKeys

--- modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/odata/entity/v1_0/VocabularyEntityModel.java
+++ modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/odata/entity/v1_0/VocabularyEntityModel.java
@@ -8,6 +8,7 @@ package com.liferay.headless.admin.taxonomy.internal.odata.entity.v1_0;
 import com.liferay.headless.common.spi.odata.entity.EntityFieldsMapFactory;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.odata.entity.CollectionEntityField;
 import com.liferay.portal.odata.entity.DateTimeEntityField;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
@@ -22,6 +23,18 @@ public class VocabularyEntityModel implements EntityModel {
 
 	public VocabularyEntityModel() {
 		_entityFieldsMap = EntityFieldsMapFactory.create(
+			new CollectionEntityField(
+				new StringEntityField(
+					"assetLibraryKeys", null,
+					locale -> Field.getSortableFieldName(
+						"assetLibraryKeys_".concat(
+							LocaleUtil.toLanguageId(locale))))),
+			new CollectionEntityField(
+				new StringEntityField(
+					"assetTypes", null,
+					locale -> Field.getSortableFieldName(
+						"assetTypes_".concat(
+							LocaleUtil.toLanguageId(locale))))),
 			new DateTimeEntityField(
 				"dateCreated",
 				locale -> Field.getSortableFieldName(Field.CREATE_DATE),
