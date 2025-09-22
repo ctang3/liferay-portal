8dcbb1f11453dca9e4df1e91ef52fbe60f86d9e1
Cheryl Tang <cheryl.tang@liferay.com>
2025-09-22 16:22:00 -0700

    LPD-65298 Implement the link for the empty page preview's edit button

--- modules/apps/layout/layout-type-controller/layout-type-controller-empty/src/main/java/com/liferay/layout/type/controller/empty/internal/layout/type/controller/EmptyLayoutTypeController.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-empty/src/main/java/com/liferay/layout/type/controller/empty/internal/layout/type/controller/EmptyLayoutTypeController.java
@@ -5,6 +5,7 @@
 
 package com.liferay.layout.type.controller.empty.internal.layout.type.controller;
 
+import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
 import com.liferay.layout.type.controller.BaseLayoutTypeControllerImpl;
 import com.liferay.petra.io.unsync.UnsyncStringWriter;
 import com.liferay.petra.string.StringPool;
@@ -12,10 +13,16 @@ import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutTypeController;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.service.permission.LayoutPermission;
 import com.liferay.portal.kernel.servlet.PipingServletResponse;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.portlet.PortletRequest;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletResponse;
@@ -68,6 +75,45 @@ public class EmptyLayoutTypeController extends BaseLayoutTypeControllerImpl {
 
 			throw new NoSuchLayoutException();
 		}
+
+		String backURL = ParamUtil.getString(
+			_portal.getOriginalServletRequest(httpServletRequest),
+			"p_l_back_url", themeDisplay.getURLCurrent());
+
+		httpServletRequest.setAttribute(
+			"editURL",
+			PortletURLBuilder.create(
+				_portal.getControlPanelPortletURL(
+					httpServletRequest, LayoutAdminPortletKeys.GROUP_PAGES,
+					PortletRequest.RENDER_PHASE)
+			).setMVCRenderCommandName(
+				"/layout_admin/select_layout_page_template_entry"
+			).setRedirect(
+				() -> {
+					String redirect = ParamUtil.getString(
+						httpServletRequest, "redirect");
+
+					if (Validator.isNull(redirect)) {
+						redirect = _portal.escapeRedirect(backURL);
+					}
+
+					return redirect;
+				}
+			).setBackURL(
+				backURL
+			).setParameter(
+				"editAction", Boolean.TRUE
+			).setParameter(
+				"externalReferenceCode", layout.getExternalReferenceCode()
+			).setParameter(
+				"groupId", layout.getGroupId()
+			).setParameter(
+				"initialType", LayoutConstants.TYPE_EMPTY
+			).setParameter(
+				"privateLayout", layout.isPrivateLayout()
+			).setParameter(
+				"selPlid", layout.getPlid()
+			).buildString());
 
 		return super.includeLayoutContent(
 			httpServletRequest, httpServletResponse, layout);
@@ -131,6 +177,9 @@ public class EmptyLayoutTypeController extends BaseLayoutTypeControllerImpl {
 
 	@Reference
 	private LayoutPermission _layoutPermission;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference(
 		target = "(osgi.web.symbolicname=com.liferay.layout.type.controller.empty)"
