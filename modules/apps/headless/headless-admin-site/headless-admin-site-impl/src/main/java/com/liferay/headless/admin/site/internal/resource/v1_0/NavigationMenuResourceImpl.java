17eaa7d9f1399721dc1736d62d571907d10f1f8b
Cheryl Tang <cheryl.tang@liferay.com>
2025-12-19 12:51:26 -0800

    LPD-72676 Remove _getLayout method since we no longer have the layoutUuid typeSetting property

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/NavigationMenuResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/NavigationMenuResourceImpl.java
@@ -381,17 +381,6 @@ public class NavigationMenuResourceImpl
 		}
 	}
 
-	private Layout _getLayout(SiteNavigationMenuItem siteNavigationMenuItem) {
-		UnicodeProperties unicodeProperties = _getUnicodeProperties(
-			siteNavigationMenuItem);
-
-		String externalReferenceCode = unicodeProperties.get(
-			"externalReferenceCode");
-
-		return _layoutLocalService.fetchLayoutByExternalReferenceCode(
-			externalReferenceCode, siteNavigationMenuItem.getGroupId());
-	}
-
 	private Locale _getLocaleFromProperty(Map.Entry<String, String> property) {
 		return LocaleUtil.fromLanguageId(
 			StringUtil.removeSubstring(property.getKey(), "name_"));
@@ -996,10 +985,12 @@ public class NavigationMenuResourceImpl
 		SiteNavigationMenuItem siteNavigationMenuItem,
 		Map<Long, List<SiteNavigationMenuItem>> siteNavigationMenuItemsMap) {
 
-		Layout layout = _getLayout(siteNavigationMenuItem);
-
 		UnicodeProperties unicodeProperties = _getUnicodeProperties(
 			siteNavigationMenuItem);
+
+		Layout layout = _layoutLocalService.fetchLayoutByExternalReferenceCode(
+			unicodeProperties.getProperty("externalReferenceCode"),
+			siteNavigationMenuItem.getGroupId());
 
 		String navigationMenuItemType = _toType(
 			siteNavigationMenuItem.getType());
